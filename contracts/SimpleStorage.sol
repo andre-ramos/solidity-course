7f54cf077fd409b940246a9b6e81f5fc42b1e4b0
"Andre Ramos" <"andre.rammos@outlook.com">
2022-03-27 14:48:12 -0300

    first commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.7;
+
+contract SimpleStorage {
+    
+    // unsigned int
+    uint256 favoritenumber = 5;
+
+    int256 favorint = -5;
+
+    address adrresfavor = 0x4324324324234;
+
+    bool favoritebool = false;
+
+    string stringm = "Hello World";
+
+
+
+}
