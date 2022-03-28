9277ed4d06ded3ad3ef94e609ee1f729498e64fc
"Andre Ramos" <"andre.rammos@outlook.com">
2022-03-28 17:46:32 -0300

    first smart contract

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # solidity-course
 https://www.youtube.com/watch?v=M576WGiDBdQ
+
+Used Remix IDE with DGIT plugin
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,18 +1,49 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.7;
 
 contract SimpleStorage {
     
     // unsigned int
-    uint256 favoritenumber = 5;
+    uint256 favoritenumber;
+    bool favbool;
 
+
+    struct People {
+        uint256 favoritenumber;
+        string name;        
+    }
+
+    People[] public people;
+    // string map to uint256
+    mapping(string => uint256) public nameToFavNumber;
+
+    // storage : variable data will persist after function executes
+    // memory: after execution deletes variable data
+    // you must use string memory to work
+    function addPerson(string memory _name, uint256 _favnumber) public{
+        people.push(People(_favnumber,_name));
+        nameToFavNumber[_name] = _favnumber;
+    }
+
+    function store(uint256 _favnumber) public {
+        favoritenumber = _favnumber;
+    }
+
+
+
+    // Both below does not modify the blockchain
+    // view keyword only reads from the blockchain, without transact
+    // pure keyword, only does math for example but it does not save
+    function retrieve() public view returns(uint256) {
+        return favoritenumber;
+    }
+    /*
     int256 favorint = -5;
-
-    address adrresfavor = 0x4324324324234;
-
+    address adrresfavor = 0x8d6cF2Fd948C29cC41D4F835f14834d7cdaC853C;
     bool favoritebool = false;
-
     string stringm = "Hello World";
-
-
+    bytes32 fabytes = "cat";
+    */
 
 }
